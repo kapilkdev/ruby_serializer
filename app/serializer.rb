07624d4c3de2304e7f64693dd5a5bab6f04f3cae
kapil <kapilkpatel03@gmail.com>
2022-07-04 21:14:36 +0530

    Handle serializer attribute

--- app/serializer.rb
+++ app/serializer.rb
@@ -1,2 +1,16 @@
 class Serializer
+
+  class << self
+    @@_attribute = {}
+
+    def attribute(*attrs, &block)
+      attrs.each do |attr|
+        attr_hash[attr.to_s] ||= block_given? ? block : attr.freeze
+      end
+    end
+
+    def attr_hash
+      @@_attribute[self.name] ||= {}
+    end
+  end
 end
